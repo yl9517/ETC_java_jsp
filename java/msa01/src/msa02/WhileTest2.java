a11b85fefb9351838bb8e89544968a24845f5349
yl9517 <80736033+yl9517@users.noreply.github.com>
2021-06-01 18:24:55 +0900

    2일차 - 반복문(switch,for,while)

---
+++ java/msa01/src/msa02/WhileTest2.java
@@ -0,0 +1,24 @@
+package msa02;
+
+public class WhileTest2 {
+
+	public static void main(String[] args) {
+		
+		int i=10;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<10);
+		
+		
+		i=1;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=5);
+	
+		
+
+	}
+
+}
