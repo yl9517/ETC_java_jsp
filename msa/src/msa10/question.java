e12aaae97466b9db5d8af5cfa7ed4ed13fc05672
yl9517 <80736033+yl9517@users.noreply.github.com>
2021-06-12 23:50:37 +0900

    10일차 과제 업뎃

--- msa/src/msa10/question.java
+++ msa/src/msa10/question.java
@@ -1,4 +1,4 @@
-package msa10;
+package msa;
 /*
  * Main에서 구현한 배열을 이용한 성적관리 프로그램을 memberPro에서 하도록 프로그램을 변경하라.
  * 
@@ -28,19 +28,118 @@ class MemberDTO{
 	private int tot;
 	private float avg;
 	
+	public MemberDTO() {
+		
+	}
 	public MemberDTO(String name,int kor,int eng, int tot, float avg) {
 		this.name = name;
 		this.kor = kor;
 		this.eng = eng;
 		this.tot = tot;
 		this.avg = avg;
+		
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getTot() {
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+
+	public float getAvg() {
+		return avg;
+	}
+
+	public void setAvg(float avg) {
+		this.avg = avg;
+	}
+	
+	
 	
 }
 
 class MemberPro{
+	
 	private MemberDTO[] arr = new MemberDTO[10];
 	//객체를 추가하고 정렬하고 출력하는 각각의 메소드를 구현
+	
+	//계산 및 객체생성
+	public void add(String name,int kor,int eng) {
+		//총점,평균계산 
+		int tot = kor+eng;
+		float avg = tot/2.0f;
+		
+		//해당자리 널이면 객체 생성
+		for(int i=0; i<arr.length; i++) { //배열 자리만큼 돌고
+			if(arr[i]==null) { //i번째가 널이면 값 넣기
+				arr[i]= new MemberDTO(name, kor, eng, tot, avg); 
+				break; //값 넣었으면 빠져나오기
+			}
+		}
+		
+	}
+	
+	//내림차순
+	public void sort() {
+		
+		MemberDTO temp;
+		for(int i=0; i<arr.length-1; i++) {
+			
+			if(arr[i]==null)
+				break; //i번째가 null이면 빠져나오기
+		
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[j]==null)
+					break; //j번째가 null이면 빠져나오기
+				
+				if(arr[i].getTot()<arr[j].getTot()) {//총점 기준으로 자리 바꾸기
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+				
+			}
+		}
+		
+	} //end sort method
+	
+	//널값아닐 시 출력
+	public void print() {
+			System.out.printf("이름\t 국어\t 영어\t 총점\t 평균\n");
+			
+		for(MemberDTO i:arr) {
+			if(i!=null) {
+				System.out.printf("%s\t %d\t %d\t %d\t %.2f\n",i.getName(),i.getKor(),i.getEng(),i.getTot(),i.getAvg());
+			}
+				
+		}
+	}
 	
 }
 public class question {
@@ -48,7 +147,11 @@ public class question {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); //main에서 객체를 입력하도록
 		//memberPro에서 구현한 메소드를 호출하여 출력
-		for(int i=0; i<10; i++) {
+		
+		MemberPro mp = new MemberPro();
+		
+		int i=0;
+		while(i<10) {
 			System.out.print("이름 : ");
 			String name = sc.next();
 			System.out.print("국어 : ");
@@ -56,7 +159,21 @@ public class question {
 			System.out.print("영어 : ");
 			int eng = sc.nextInt();
 			
+			mp.add(name, kor, eng);
+			
+			i++;
+			if(i<10) {
+				System.out.print("0 - 입력 끝내기 | 1 - 계속입력 :");
+				int num=sc.nextInt();
+				
+				if(num==0) //0누르면 break
+					break;
+			}
 		}
+		
+		mp.sort(); //내림차순
+		
+		mp.print(); //출력
 
 	}
 
